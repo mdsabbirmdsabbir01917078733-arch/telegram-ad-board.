ce4ab2734431298e62bcbab19d08d9c91108a85e
mdsabbirmdsabbir01917078733-arch <mdsabbirmdsabbir01917078733@gmail.com>
2026-01-08 10:40:25 +0600

    admin/firebase.js

--- admin/firebase.js
+++ admin/firebase.js
@@ -1,22 +1,18 @@
-<!-- Firebase SDKs -->
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-database-compat.js"></script>
+// Firebase Core
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-<script>
-  // 🔥 Firebase Configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyBNcV0FMuEvxgg6FFClJ5r7Xld4g3Z7x6U",
-    authDomain: "double-earning-bot.firebaseapp.com",
-    databaseURL: "https://double-earning-bot-default-rtdb.firebaseio.com",
-    projectId: "double-earning-bot",
-    storageBucket: "double-earning-bot.firebasestorage.app",
-    messagingSenderId: "155076870534",
-    appId: "1:155076870534:web:0023206fbb40f56aafac07"
-  };
+// 🔥 Your Firebase Config (তুমি যেটা দিয়েছো)
+const firebaseConfig = {
+  apiKey: "AIzaSyBNcV0FMuEvxgg6FFClJ5r7Xld4g3Z7x6U",
+  authDomain: "double-earning-bot.firebaseapp.com",
+  databaseURL: "https://double-earning-bot-default-rtdb.firebaseio.com",
+  projectId: "double-earning-bot",
+  storageBucket: "double-earning-bot.firebasestorage.app",
+  messagingSenderId: "155076870534",
+  appId: "1:155076870534:web:0023206fbb40f56aafac07",
+};
 
-  // 🔥 Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
-  // 🔥 Database reference
-  const db = firebase.database();
-</script>
+// Init
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
